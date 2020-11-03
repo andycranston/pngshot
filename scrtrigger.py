15586d7e6ae091ddda19a76a06d14d7b3125eefe
andycranston <acranston@gmail.com>
2020-11-03 17:33:33 +0000

    Removed main function so code cleaner at level 0

--- scrtrigger.py
+++ scrtrigger.py
@@ -1,58 +1,38 @@
 #! /usr/bin/python3
 #
-# @(!--#) @(#) scrtrigger.py, version 002, 01-october-2020
+# @(!--#) @(#) scrtrigger.py, sversion 0.1.0, version 003, 03-december-2020
 #
-# trigger a screenshot by sending a UDP packet to
-# the scrshot.py program
+# trigger a screenshot by sending a UDP packet to the pngshot.py program
 #
-
-########################################################################
 
 import sys
 import os
 import socket
 
-########################################################################
-
 DEFAULT_UDP_PORT = 8333
 
-########################################################################
+if len(sys.argv) > 1:
+    hostandudp = sys.argv[1]
+else:
+    hostandudp = '127.0.0.1'
 
-#
-# Main
-#
+fields = hostandudp.split(':')
 
-def main():
-    global progname
+host = fields[0]
 
-    if len(sys.argv) > 1:
-        hostandudp = sys.argv[1]
-    else:
-        hostandudp = '127.0.0.1'
+if len(fields) >= 2:
+    udp = int(fields[1])
+else:
+    udp = DEFAULT_UDP_PORT
 
-    fields = hostandudp.split(':')
+print('Triggering screenshot on host {} using UDP port {}'.format(host, udp))
 
-    host = fields[0]
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.sendto('please take a screenshot now'.encode(), (host, udp))
+sock.close()
 
-    if len(fields) >= 2:
-        udp = int(fields[1])
-    else:
-        udp = DEFAULT_UDP_PORT
+print('Done')
 
-    print('Triggering screenshot on host {} using UDP port {}'.format(host, udp))
-
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.sendto('please take a screenshot now'.encode(), (host, udp))
-    sock.close()
-
-    print('Done')
-
-    return 0
-
-########################################################################
-
-progname = os.path.basename(sys.argv[0])
-
-sys.exit(main())
+sys.exit(0)
 
 # end of file
